118cc26b51348ab67fc3710d53460f4d0fdea0c8
Quillinan <milton.quillinan@gmail.com>
2023-09-21 03:40:27 -0300

    add aula 10

---
+++ aula-prisma-10/enunciado__donos-animais/src/index.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response } from "express";
+import prisma from "./database";
+import { Prisma } from "@prisma/client";
+
+const app = express();
+
+type PetResult = {
+  owner: string;
+  pet: string;
+  animal: string;
+};
+
+app.get("/pets/owner/:ownerName", async (req: Request, res: Response) => {
+  const { ownerName } = req.params;
+  try {
+    const result = await prisma.$queryRaw<PetResult[]>(
+      Prisma.sql`
+      SELECT pet.*
+      FROM "Pet" pet
+      JOIN "Person" person ON pet.personId = person.id
+      WHERE person.name = $1
+    `,
+      ownerName
+    );
+
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  } finally {
+    await prisma.$disconnect();
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is up and running or port ${port}`);
+});
